b6b049b0fb041c284ce7f2f0f8716a210005e7ec
Nick <i@httpd.cc>
2021-12-27 21:11:13 +0800

    debug

--- src/User.php
+++ src/User.php
@@ -15,6 +15,10 @@ class User implements AuthenticatableInterface
     public function __construct($user)
     {
         $this->info = $user;
+
+        logger()->info('debug user', [
+            $user
+        ]);
     }
 
     /**
